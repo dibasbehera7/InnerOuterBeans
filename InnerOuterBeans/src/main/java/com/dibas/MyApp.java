23af06f872d519016a10a2fe5d627f807a515666
dibasbehera7 <dibasbehera@gmail.com>
2018-12-24 22:22:54 +0530

    Setting Init and Destroy Methods on Beans

--- InnerOuterBeans/src/main/java/com/dibas/MyApp.java
+++ InnerOuterBeans/src/main/java/com/dibas/MyApp.java
@@ -3,6 +3,7 @@ package com.dibas;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+import com.dibas.food.Fruit;
 import com.dibas.food.Meal;
 
 public class MyApp {
@@ -11,14 +12,17 @@ public class MyApp {
 
 		ApplicationContext appContext = new FileSystemXmlApplicationContext("appContext.xml");
 
-		//Meal mySpringMeal = (Meal) appContext.getBean("meal");
-		//System.out.println(mySpringMeal.whatsInTodaysMeal());
-		
-		Meal myInnerBeanAppleMeal = appContext.getBean("mealOuterBean",Meal.class);
-		System.out.println("Inner Bean\n"+myInnerBeanAppleMeal.whatsInTodaysMeal());
-		
+		// Meal mySpringMeal = (Meal) appContext.getBean("meal");
+		// System.out.println(mySpringMeal.whatsInTodaysMeal());
 
-		
+		/*
+		 * Meal myInnerBeanAppleMeal = appContext.getBean("mealOuterBean",Meal.class);
+		 * System.out.println("Inner Bean\n"+myInnerBeanAppleMeal.whatsInTodaysMeal());
+		 */
+
+		Fruit myFruit = appContext.getBean("fruit", Fruit.class);
+		System.out.println(myFruit.talkAboutYou());
+
 		
 		
 		((FileSystemXmlApplicationContext) appContext).close();
--- InnerOuterBeans/src/main/java/com/dibas/food/Fruit.java
+++ InnerOuterBeans/src/main/java/com/dibas/food/Fruit.java
@@ -2,14 +2,31 @@ package com.dibas.food;
 
 public class Fruit {
 
+	private String description = "not set";
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 	public Fruit() {
 		// no arg constructor
 	}
 
 	public String talkAboutYou() {
-
-		return "Hi, I'm Fruit. ";
-
+		return description;
 	}
-		
+	
+	
+	public void initMethod() {
+		System.out.println("The fruit bean is ready to go. "+description);
+	}
+	
+	public void destroyMethod() {
+		System.out.println("The fruit bean is about to be destroyed.");
+	}
+
 }
